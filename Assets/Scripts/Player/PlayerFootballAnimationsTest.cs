9bc22a6dd157b7e795db759b67e95e8e6fdcaa29
betzabxscobar <betzabxscobar@gmail.com>
2026-07-07 14:32:12 -0500

    Primera Animacion Jugador Shoot

--- Assets/Scripts/Player/PlayerFootballAnimationsTest.cs
+++ Assets/Scripts/Player/PlayerFootballAnimationsTest.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerFootballAnimationsTest : MonoBehaviour
 {
     private Animator animator;
+
+    [Header("Nombre exacto de la escena de la cancha")]
+    [SerializeField] private string canchaSceneName = "minijuego1";
 
     private void Awake()
     {
@@ -13,8 +17,14 @@ public class PlayerFootballAnimationsTest : MonoBehaviour
     {
         if (animator == null) return;
 
+        if (SceneManager.GetActiveScene().name != canchaSceneName)
+        {
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.O))
         {
+            Debug.Log("SHOOT ACTIVADO");
             animator.SetTrigger("Shoot");
         }
     }
